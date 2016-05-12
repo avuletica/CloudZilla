f2383f466e57d2e8de6f5a486cc9d69b6940143b
Bcoolie <vuletic23@gmail.com>
2016-05-12 16:44:03 +0200

    Override label text

--- cloud/forms.py
+++ cloud/forms.py
@@ -6,3 +6,7 @@ class FileUploadForm(forms.ModelForm):
     class Meta:
         model = FileUpload
         fields = ['file']
+
+    def __init__(self, *args, **kwargs):
+        super(FileUploadForm, self).__init__(*args, **kwargs)
+        self.fields['file'].label = "Choose a file"
